629bcdd9cb258315ee62f6486891c6d8a4a2ee7b
guilhermejardim <guilherme.lima@ab-inbev.com>
2025-03-19 10:08:32 -0300

    update database 3

--- src/main/kotlin/database/DatabaseFactory.kt
+++ src/main/kotlin/database/DatabaseFactory.kt
@@ -32,9 +32,23 @@ object DatabaseFactory {
             ?: "postgresql://fdm_db_user:4vRl9uTv7PeJlcGTJVTV3A9kkjJA7TNC@dpg-cvd3lc3v2p9s73cb5hl0-a.oregon-postgres.render.com/fdm_db"
 
         if (System.getenv("RENDER") != null || databaseUrl.contains("render.com")) {
-            val jdbcUrl = databaseUrl.replace("postgresql://", "jdbc:postgresql://")
+            val regex = Regex("postgres(?:ql)?:\\/\\/(?<user>[^:]+):(?<password>[^@]+)@(?<host>[^:/]+)(:(?<port>\\d+))?/(?<database>.+)")
+            val matchResult = regex.matchEntire(databaseUrl)
 
-            config.jdbcUrl = jdbcUrl
+            if (matchResult != null) {
+                val groups = matchResult.groups
+                val user = groups["user"]?.value ?: ""
+                val password = groups["password"]?.value ?: ""
+                val host = groups["host"]?.value ?: ""
+                val port = groups["port"]?.value ?: "5432"  // Assume 5432 se não tiver
+                val database = groups["database"]?.value ?: ""
+
+                config.jdbcUrl = "jdbc:postgresql://$host:$port/$database"
+                config.username = user
+                config.password = password
+            } else {
+                throw RuntimeException("DATABASE_URL format is invalid")
+            }
         } else {
             config.jdbcUrl = "jdbc:postgresql://localhost:5432/peladamanager"
             config.username = "fdm_db_user"
