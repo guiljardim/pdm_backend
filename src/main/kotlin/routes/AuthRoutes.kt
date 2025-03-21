53d9b3e6b6056117fcbdeb006ca1ef49def94c9c
guilhermejardim <guilherme.lima@ab-inbev.com>
2025-03-20 21:46:29 -0300

    correction in authRoutes

--- src/main/kotlin/routes/AuthRoutes.kt
+++ src/main/kotlin/routes/AuthRoutes.kt
@@ -11,12 +11,16 @@ import models.User
 import org.example.services.AuthService
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import org.example.services.JogadorService
 import java.util.Date
 
 private val authService = AuthService()
+private val jogadorService = JogadorService()
 
 fun Route.authRoutes() {
     route("/auth") {
+
+        // LOGIN
         post("/login") {
             val loginRequest = call.receive<LoginRequest>()
             val user = authService.getUser(loginRequest.username, loginRequest.password)
@@ -25,20 +29,28 @@ fun Route.authRoutes() {
                 call.respond(HttpStatusCode.Unauthorized, "Credenciais inválidas")
                 return@post
             }
+            val jogador = user.jogadorId?.let { jogadorService.getJogadorById(it) }
 
             val token = JWT.create()
                 .withAudience("varzino")
                 .withIssuer("varzinoApp")
                 .withClaim("username", user.username)
                 .withClaim("userId", user.id)
-                .withExpiresAt(Date(System.currentTimeMillis() + 6000000)) // expira em ~100 minutos
+                .withExpiresAt(Date(System.currentTimeMillis() + 6000000))
                 .sign(Algorithm.HMAC256("sua-chave-secreta"))
 
-            call.respond(LoginResponse(token, user.copy(password = "")))
+            call.respond(LoginResponse(user, token, jogador))
         }
 
+        // REGISTER
         post("/register") {
             val user = call.receive<User>()
+
+            if (user.jogadorId == null) {
+                call.respond(HttpStatusCode.BadRequest, "jogadorId é obrigatório")
+                return@post
+            }
+
             val createdUser = authService.createUser(user)
             call.respond(HttpStatusCode.Created, createdUser.copy(password = ""))
         }
